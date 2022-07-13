5eafe66681afb55788374e0ecbc3041a9458e4c1
SergioCastro02 <sergiohmcastro02@gmail.com>
2022-07-12 22:17:49 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificacionButton from "./components/NotificationButton"
+
 function App() {
-  return(
-    <h1>Olá mundo!</h1>
+  return (
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificacionButton />
+    </>
   )
 }
 
